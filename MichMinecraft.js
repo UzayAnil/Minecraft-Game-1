136a6d1ea11bc9ebd6985a6cf3358801355a8a35
michelelang <michelelang3@gmail.com>
2017-12-27 14:08:07 +0200

    initiallizing the repository

---
+++ MichMinecraft.js
@@ -0,0 +1,17 @@
+
+
+
+for (var row = 0; row < 19; row++) {
+	for (var col = 0; col < 15; col++) {
+		var tiles = $("<div/>")
+			.addClass('tile')
+			.appendTo($('#mainBoard'));
+	}
+}
+
+
+
+
+
+
+
