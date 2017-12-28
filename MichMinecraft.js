56124182eb8ae655975ffa7b0c45043f5bd45afb
michelelang <michelelang3@gmail.com>
2017-12-28 11:10:32 +0200

    matrix array added and grid

--- MichMinecraft.js
+++ MichMinecraft.js
@@ -1,21 +1,34 @@
 var mineCraft = {};
 
 
+
+
 mineCraft.init = function () {
 	mineCraft.createBoard();
 }
 
 
 mineCraft.createBoard = function () {
-	for (var row = 0; row < 19; row++) {
-		for (var col = 0; col < 15; col++) {
-			mineCraft.tiles = $("<div/>")
-				.addClass('tile')
-				.appendTo($('#mainBoard'));
-		}
-		console.log("hello");
+
+	mineCraft.matrix = new Array(21);
+	for (var i = 0; i < mineCraft.matrix.length; i++) {
+		mineCraft.matrix[i] = new Array(21);
+		//creating 20 by 20 matrix array
 	}
-}
+	console.log(mineCraft.matrix);
+
+		for (var x = 0; x < mineCraft.matrix.length; x++) {
+			for (var y = 0; y < mineCraft.matrix.length; y++) {
+				mineCraft.matrix[x][y] = "";
+				var box = $("<div/>")
+				.addClass("box")
+				$('#mainBoard').append(box);
+		}
+			}
+	
+	
+	}
+
 
 $(document).ready(function() {
     mineCraft.init();
