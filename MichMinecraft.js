e976161602f6fc9f3ebf3832442337267a2ebd00
michelelang <michelelang3@gmail.com>
2017-12-29 12:52:42 +0200

    changes

--- MichMinecraft.js
+++ MichMinecraft.js
@@ -1,7 +1,7 @@
 var mineCraft = {};
 
 
-($(".tool")).click(function(){
+($(".tool")).click(function () {
 	console.log("Hello!");
 })
 
@@ -11,7 +11,7 @@ mineCraft.init = function () {
 
 //adding color indicator 
 var colorIndicator = $("<div/>")
-.addClass("box skyblue backgroundIndicator")
+	.addClass("box skyblue backgroundIndicator")
 $(".col-lg-3").append(colorIndicator);
 
 mineCraft.createBoard = function () {
@@ -25,31 +25,31 @@ mineCraft.createBoard = function () {
 		for (var y = 0; y < mineCraft.matrix.length; y++) {
 			var box = $("<div/>")
 				.addClass("box skyblue");
-			if (x==14){
-				box.addClass("dirtWithGrass");		
+			if (x == 14) {
+				box.addClass("dirtWithGrass");
 			}
 
 			//adding rocks
-			else if ((x === 13 && y === 13) || (x === 13 && y === 14) || (x===13 && y ===20)) {
+			else if ((x === 13 && y === 13) || (x === 13 && y === 14) || (x === 13 && y === 20)) {
 				box.addClass("rock");
 			}
 			//adding tree bark
-			else if ((x === 13&&y===16) || (x === 12&&y===16) || (x === 11&&y===16)) {
+			else if ((x === 13 && y === 16) || (x === 12 && y === 16) || (x === 11 && y === 16)) {
 				box.addClass("treeBark");
 			}
 			//adding tree leaves
-			else if ((x === 8 && y===17)|| (x===10 && y === 17) || (x === 9 && y===17) || (x === 10 && y===15)||(x === 8 && y===15)||(x === 9 && y===15)
-			||(x === 8 && y===15)||(x === 8 && y===16)||(x === 9 && y===16)||(x === 10&&y===16)) {
-	
+			else if ((x === 8 && y === 17) || (x === 10 && y === 17) || (x === 9 && y === 17) || (x === 10 && y === 15) || (x === 8 && y === 15) || (x === 9 && y === 15)
+				|| (x === 8 && y === 15) || (x === 8 && y === 16) || (x === 9 && y === 16) || (x === 10 && y === 16)) {
+
 				box.addClass("treeLeaf");
 			}
 			//adding bush
-			else if((x===13&&y===1) || (x===13&&y===2)||(x===13&&y===3)||(x===13&&y===4)||(x===12&&y===2)||(x===12&&y===3)) {
+			else if ((x === 13 && y === 1) || (x === 13 && y === 2) || (x === 13 && y === 3) || (x === 13 && y === 4) || (x === 12 && y === 2) || (x === 12 && y === 3)) {
 				box.addClass("justGrass");
 			}
 
 			//adding the cloud
-			else if((x===4&&y===4)||(x===4&&y===5) ||(x===4&&y===6) || (x===4&&y===7)|| (x===4&&y===8) || (x===3&&y===5) || (x===3&&y===6) || (x===3&&y===7)||(x===5&&y===6)||(x===5&&y===5)||(x===5&&y===7)){
+			else if ((x === 4 && y === 4) || (x === 4 && y === 5) || (x === 4 && y === 6) || (x === 4 && y === 7) || (x === 4 && y === 8) || (x === 3 && y === 5) || (x === 3 && y === 6) || (x === 3 && y === 7) || (x === 5 && y === 6) || (x === 5 && y === 5) || (x === 5 && y === 7)) {
 				box.removeClass("skyblue");
 				box.addClass("cloud");
 			}
@@ -74,7 +74,7 @@ $(document).ready(function () {
 
 });
 
-whatever=function(){
+whatever = function () {
 	console.log("whatever");
 }
 
