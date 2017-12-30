8ea9d396aef875c0d6634a39416af859e90088c9
Aaron Kammerman <aaron@aarons-mbp.home>
2017-12-31 00:07:21 +0200

    Properly set the background indicator function

--- MichMinecraft.js
+++ MichMinecraft.js
@@ -2,12 +2,12 @@ var mineCraft = {};
 
 
 ($(".tool")).click(function () {
-	console.log("Hello!");
+    console.log("Hello!");
 })
 
 mineCraft.init = function () {
-	mineCraft.createBoard();
-	toolCreator();
+    mineCraft.createBoard();
+    toolCreator();
 }
 
 
@@ -15,20 +15,20 @@ var toolNames = ["pickaxe", "shovel", "axe"];
 var toolPics = ["Pickaxe.png", "Shovel.png", "Axe.png"];
 
 function toolCreator() {
-	for (var i = 0; i < toolNames.length; i++) {
-		var div = $("<div/>");
-		div.addClass("tool " + toolNames[i]);
-		var image = $("<img/>");
-		image.addClass("toolImage");
-		image.attr("src", toolPics[i]);
-		div.append(image);
-		var toolText = $("<p/>");
-		toolText.text(toolNames[i]);
-		$(".textToolBar").append(div);
-		div.append(toolText);
-		div.on("click", toolClick);
-		$("p").css("font-size", "10px");
-	}
+    for (var i = 0; i < toolNames.length; i++) {
+        var div = $("<div/>");
+        div.addClass("tool " + toolNames[i]);
+        var image = $("<img/>");
+        image.addClass("toolImage");
+        image.attr("src", toolPics[i]);
+        div.append(image);
+        var toolText = $("<p/>");
+        toolText.text(toolNames[i]);
+        $(".textToolBar").append(div);
+        div.append(toolText);
+        div.on("click", toolClick);
+        $("p").css("font-size", "10px");
+    }
 }
 
 //Initalized the variable and created click event
@@ -36,135 +36,144 @@ var currentTool;
 var hasTool;
 
 function toolClick() {
-	currentTool = $(this);
-	hasTool = currentTool.attr("class");
-	currentTool.toggleClass("toolBackgroundColor");
-	console.log(hasTool);
+    currentTool = $(this);
+    hasTool = currentTool.attr("class");
+    currentTool.toggleClass("toolBackgroundColor");
 }
 
 
 //adding color indicator 
 var colorIndicator = $("<div/>")
-	.addClass("box skyblue backgroundIndicator");
+.addClass("box skyblue backgroundIndicator");
 $(".col-lg-3").append(colorIndicator);
 
 mineCraft.createBoard = function () {
-	mineCraft.matrix = new Array(21);
-	for (var i = 0; i < mineCraft.matrix.length; i++) {
-		mineCraft.matrix[i] = new Array(21);
-		//creating 20 by 20 matrix array
-	}
+    mineCraft.matrix = new Array(21);
+    for (var i = 0; i < mineCraft.matrix.length; i++) {
+        mineCraft.matrix[i] = new Array(21);
+        //creating 20 by 20 matrix array
+    }
 
-	for (var x = 0; x < mineCraft.matrix.length; x++) {
-		for (var y = 0; y < mineCraft.matrix.length; y++) {
-			var box = $("<div/>")
-				.addClass("box skyblue");
-			if (x == 14) {
-				box.addClass("dirtWithGrass");
-			}
+    for (var x = 0; x < mineCraft.matrix.length; x++) {
+        for (var y = 0; y < mineCraft.matrix.length; y++) {
+            var box = $("<div/>")
+                .addClass("box skyblue");
+            if (x == 14) {
+                box.addClass("dirtWithGrass");
+            }
 
-			//adding rocks
-			else if ((x === 13 && y === 13) || (x === 13 && y === 14) || (x === 13 && y === 20)) {
-				box.addClass("rock");
-			}
-			//adding tree bark
-			else if ((x === 13 && y === 16) || (x === 12 && y === 16) || (x === 11 && y === 16)) {
-				box.addClass("treeBark");
-			}
-			//adding tree leaves
-			else if ((x === 8 && y === 17) || (x === 10 && y === 17) || (x === 9 && y === 17) || (x === 10 && y === 15) || (x === 8 && y === 15) || (x === 9 && y === 15)
-				|| (x === 8 && y === 15) || (x === 8 && y === 16) || (x === 9 && y === 16) || (x === 10 && y === 16)) {
+            //adding rocks
+            else if ((x === 13 && y === 13) || (x === 13 && y === 14) || (x === 13 && y === 20)) {
+                box.addClass("rock");
+            }
+            //adding tree bark
+            else if ((x === 13 && y === 16) || (x === 12 && y === 16) || (x === 11 && y === 16)) {
+                box.addClass("treeBark");
+            }
+            //adding tree leaves
+            else if ((x === 8 && y === 17) || (x === 10 && y === 17) || (x === 9 && y === 17) || (x === 10 && y === 15) || (x === 8 && y === 15) || (x === 9 && y === 15)
+                || (x === 8 && y === 15) || (x === 8 && y === 16) || (x === 9 && y === 16) || (x === 10 && y === 16)) {
 
-				box.addClass("treeLeaf");
-			}
-			//adding bush
-			else if ((x === 13 && y === 1) || (x === 13 && y === 2) || (x === 13 && y === 3) || (x === 13 && y === 4) || (x === 12 && y === 2) || (x === 12 && y === 3)) {
-				box.addClass("justGrass");
-			}
+                box.addClass("treeLeaf");
+            }
+            //adding bush
+            else if ((x === 13 && y === 1) || (x === 13 && y === 2) || (x === 13 && y === 3) || (x === 13 && y === 4) || (x === 12 && y === 2) || (x === 12 && y === 3)) {
+                box.addClass("justGrass");
+            }
 
-			//adding the cloud
-			else if ((x === 4 && y === 4) || (x === 4 && y === 5) || (x === 4 && y === 6) || (x === 4 && y === 7) || (x === 4 && y === 8) || (x === 3 && y === 5) || (x === 3 && y === 6) || (x === 3 && y === 7) || (x === 5 && y === 6) || (x === 5 && y === 5) || (x === 5 && y === 7)) {
-				box.removeClass("skyblue");
-				box.addClass("cloud");
-			}
-			box.on("click", selectedDiv);
-			$('#mainBoard').append(box);
+            //adding the cloud
+            else if ((x === 4 && y === 4) || (x === 4 && y === 5) || (x === 4 && y === 6) || (x === 4 && y === 7) || (x === 4 && y === 8) || (x === 3 && y === 5) || (x === 3 && y === 6) || (x === 3 && y === 7) || (x === 5 && y === 6) || (x === 5 && y === 5) || (x === 5 && y === 7)) {
+                box.removeClass("skyblue");
+                box.addClass("cloud");
+            }
+            box.on("click", selectedDiv);
+            $('#mainBoard').append(box);
 
-			mineCraft.matrix[x][y] = box;
-		}
-	}
+            mineCraft.matrix[x][y] = box;
+        }
+    }
 
-	//adding dirt
-	for (var i = 15; i < mineCraft.matrix.length; i++) {
-		for (var j = 0; j < mineCraft.matrix.length; j++) {
-			mineCraft.matrix[i][j].addClass("dirt");
-		}
-	}
+    //adding dirt
+    for (var i = 15; i < mineCraft.matrix.length; i++) {
+        for (var j = 0; j < mineCraft.matrix.length; j++) {
+            mineCraft.matrix[i][j].addClass("dirt");
+        }
+    }
 };
 
 $(document).ready(function () {
-	$("#minecraftModal").modal('show');
-	mineCraft.init();
+    mineCraft.init();
 
 });
 
-
 selectedDiv = function () {
-	var divSelected = $(this).attr("class");
-	console.log(divSelected);
+    var divSelected = $(this).attr("class");
 
 
-	if (divSelected != 'box skyblue') {
+    if (divSelected != 'box skyblue') {
 
-		if ((divSelected === 'box skyblue dirt') && (hasTool === 'tool shovel')) {	
-			($('.backgroundIndicator').addClass('dirt'));
-			console.log($('.backgroundIndicator'));
-			$(this).removeClass('dirt');
-			// $('.backgroundIndicator').on("click", addDirt);
+        if ((divSelected === 'box skyblue dirt') && (hasTool === 'tool shovel')) {    
+            ($('.backgroundIndicator').data(divSelected));
+            $(this).removeClass('dirt');
+            removeClassesFromBackGroundIndicatorDiv();
+			$('.backgroundIndicator').addClass('dirt');
+        }
+
+        else if ((divSelected === 'box skyblue dirtWithGrass') && (hasTool === 'tool shovel')) {
+            ($('.backgroundIndicator').data(divSelected));
+            $(this).removeClass('dirtWithGrass');
+            removeClassesFromBackGroundIndicatorDiv();
+            $('.backgroundIndicator').addClass('dirtWithGrass');
+            
+        }
+
+        else if ((divSelected === 'box skyblue treeBark') && (hasTool === 'tool axe')) {
+            ($('.backgroundIndicator').data(divSelected));
+            $(this).removeClass('treeBark');
+            removeClassesFromBackGroundIndicatorDiv();
+            $('.backgroundIndicator').addClass('treeBark');
+        }
+
+        else if ((divSelected === 'box skyblue treeLeaf') && (hasTool === 'tool axe')) {
+            ($('.backgroundIndicator').data(divSelected));
+            $(this).removeClass('treeLeaf');
+            removeClassesFromBackGroundIndicatorDiv();
+            $('.backgroundIndicator').addClass('treeLeaf');
 		}
-
-		else if ((divSelected === 'box skyblue dirtWithGrass') && (hasTool === 'tool shovel')) {
-			($('.backgroundIndicator').data(divSelected));
-			console.log($('.backgroundIndicator'));
-			$(this).removeClass('dirtWithGrass');
-			console.log(divSelected);
-			// ($('.backgroundIndicator').removeClass("dirtWithGrass"));
-			
-		}
-
-		else if ((divSelected === 'box skyblue treeBark') && (hasTool === 'tool axe')) {
-			($('.backgroundIndicator').data(divSelected));
-			console.log($('.backgroundIndicator'));
-			$(this).removeClass('treeBark');
-			console.log(divSelected);
-			// ($('.backgroundIndicator').removeClass("treeBark"));
-		}
-
-		else if ((divSelected === 'box skyblue treeLeaf') && (hasTool === 'tool axe')) {
-			($('.backgroundIndicator').data(divSelected));
-			console.log($('.backgroundIndicator'));
-			$(this).removeClass('treeLeaf');
-			console.log(divSelected);
-			// ($('.backgroundIndicator').removeClass("treeLeaf"));
-		}
-
-		else if ((divSelected === 'box skyblue rock') && (hasTool === 'tool pickaxe')) {
-			($('.backgroundIndicator').data(divSelected));
-			console.log($('.backgroundIndicator'));
-			$(this).removeClass('rock');
-			console.log(divSelected);
-			// ($('.backgroundIndicator').removeClass("rock"));
-		}
-
+		
 		else if ((divSelected === 'box skyblue justGrass') && (hasTool === 'tool axe')) {
-			($('.backgroundIndicator').data(divSelected));
-			console.log($('.backgroundIndicator'));
-			$(this).removeClass('justGrass');
-			console.log(divSelected);
-			// ($('.backgroundIndicator').removeClass("justGrass"));
-		}
-	}
+            ($('.backgroundIndicator').data(divSelected));
+            $(this).removeClass('justGrass');
+            removeClassesFromBackGroundIndicatorDiv();
+            $('.backgroundIndicator').addClass('justGrass');
+        }
+
+        else if ((divSelected === 'box skyblue rock') && (hasTool === 'tool pickaxe')) {
+            ($('.backgroundIndicator').data(divSelected));
+            $(this).removeClass('rock');
+            removeClassesFromBackGroundIndicatorDiv();
+            $('.backgroundIndicator').addClass('rock');
+        }
+
+        else if ((divSelected === 'box skyblue justGrass') && (hasTool === 'tool pickaxe')) {
+            ($('.backgroundIndicator').data(divSelected));
+            $(this).removeClass('justGrass');
+            removeClassesFromBackGroundIndicatorDiv();
+            $('.backgroundIndicator').addClass('justGrass');
+        }
+    }
 }
+
+function removeClassesFromBackGroundIndicatorDiv() {
+    $('.backgroundIndicator').removeClass('dirt');
+    $('.backgroundIndicator').removeClass('dirtWithGrass');
+    $('.backgroundIndicator').removeClass('treeBark');
+    $('.backgroundIndicator').removeClass('treeLeaf');
+    $('.backgroundIndicator').removeClass('rock');
+    $('.backgroundIndicator').removeClass('justGrass');
+}
+
+
 
 $("#tutorialModalButton").click(function() {
 	$('.modal').css("display", "block");
@@ -183,7 +192,3 @@ $("#beginGameButton").click(function() {
 	$("#myModalIntro").css("display", "none");
 
 })
-
-
-
-
